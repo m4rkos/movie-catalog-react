01f60da10cef96b0acf982b01296319bb37981d8
marcos eduardo <marcos.j.eduardo@gmail.com>
2025-08-26 14:10:57 +0100

    implemented scss as main style files in dev mode and added tailwind

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import './Home.css'
+import './Home.scss'
 import { 
   //getPopularMovies, 
   getPopularMovies$, 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './tailwind.css';
+import './styles/index.scss'
 import App from './app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
